80850df39a69a464944a1f5a7d2a633409cc483b
Davide Betlej <davidebetlej1@gmail.com>
2020-11-07 11:52:28 +0100

    Dodano wyklady

---
+++ p6 react/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+export default class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { seconds: 0 };
+    }
+  
+    tick() {
+      this.setState(state => ({
+        seconds: state.seconds + 1
+      }));
+    }
+  
+    componentDidMount() {
+      this.interval = setInterval(() => this.tick(), 1000);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+  
+    render() {
+      return (
+        <div>
+          Sekundy: {this.state.seconds}
+        </div>
+      );
+    }
+  }
